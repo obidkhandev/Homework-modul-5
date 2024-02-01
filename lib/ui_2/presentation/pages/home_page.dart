3b65cf151b946d09600f0b35b1a73e77fd03eeef
@alwaysflutter <obidkhandev@gmail.com>
2024-02-01 17:23:18 +0500

    sing out function in ui2

--- lib/ui_2/presentation/pages/home_page.dart
+++ lib/ui_2/presentation/pages/home_page.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:modul_5/ui_2/data/services/auth/auth_service.dart';
+import 'package:provider/provider.dart';
 
-class HomePageUi2 extends StatelessWidget {
+class HomePageUi2 extends StatefulWidget {
   const HomePageUi2({super.key});
+
+  @override
+  State<HomePageUi2> createState() => _HomePageUi2State();
+}
+
+class _HomePageUi2State extends State<HomePageUi2> {
+  
+  void signOut() async {
+    final authService = Provider.of<AuthServiceUi2>(context, listen: false);
+    authService.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
+        actions: [
+          IconButton(onPressed: signOut, icon: Icon(Icons.logout),color: Colors.grey,),
+        ],
       ),
     );
   }
